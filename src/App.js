053c6355ec8983eefb740aa6fc3d55d2cfc6ec86
hillierben <hillierben@gmail.com>
2023-10-01 09:57:20 +0100

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I have made a change to this page
         </a>
       </header>
     </div>
